3cacdaa0466eb0fbecf5d93aabbd47ba388e66c1
Henadz Yermakavets <92603512+gyermakavets@users.noreply.github.com>
2025-05-28 15:09:57 -0500

    Refactor distinct array creation

--- src/main/java/com/pe/hash/FixedCharTable.java
+++ src/main/java/com/pe/hash/FixedCharTable.java
@@ -7,7 +7,16 @@ interface FixedCharTable {
     static FixedCharTable from(CharSequence charSequence) {
         if (charSequence == null || charSequence.length() == 0) return FCT0.INSTANCE;
         FCTUniversal universal = new FCTUniversal(charSequence);
-        int[] distinct = Arrays.stream(universal.chars).filter(c -> c >= 0).toArray();
+        int[] chars = universal.chars;
+        int count = 0;
+        for (int c : chars) {
+            if (c >= 0) count++;
+        }
+        int[] distinct = new int[count];
+        int i = 0;
+        for (int c : chars) {
+            if (c >= 0) distinct[i++] = c;
+        }
         if (distinct.length == 1) return new FCT1(distinct[0]);
         if (distinct.length == 2) return new FCT2(distinct[0], distinct[1]);
         FCTCuckoo randomHashPair = new FCTCuckoo(distinct, 100);
